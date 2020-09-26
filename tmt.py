03ad8c579afd15a52c2987b47553ffede19d7880
nmohnblatt <nicolas.mohnblatt@gmail.com>
2020-09-26 16:04:42 +0100

    initial commit

---
+++ tmt.py
@@ -0,0 +1,18 @@
+# from tscraper import scrape_match
+import tscraper
+import tennisrules
+import ttracker
+import sys
+import plotting
+
+
+def main(URL):
+    match = tscraper.scrape_match(URL)
+    
+    p1_tracked, p2_tracked, EoS = ttracker.trackmatch(match)
+
+    plotting.momentum_animation(match, p1_tracked, p2_tracked, EoS, save_video=True)
+
+    return 0
+
+main(str(sys.argv[1]))
